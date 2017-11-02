06bff0042c1b2be7a503353a52b2d16befb02b38
Joey Parks <josephparks13@gmail.com>
2017-11-02 11:19:27 -0400

    updated shopping page css

--- scripts/index.js
+++ scripts/index.js
@@ -2,34 +2,89 @@
 
 //Master Item Array
 var masterItemList = [
-	{
-		name: 'Grape Juice',
-		price: 4,
-		class: 'juice',
-		description: 'The purest juice from delicious grapes.',
-		itemNumber: 1
-	},
-	{
-		name: 'Apple Juice',
-		price: 3,
-		class: 'juice',
-		description: 'We squeezed an apple into a cup!',
-		itemNumber: 2
-	},
-	{
-		name: 'Kiwi Lime Blitz',
-		price: 5,
-		class: 'smoothie',
-		description: 'Savory blend of kiwis, lime juice, and sorbet.',
-		itemNumber: 3
-	},
-	{
-		name: 'Mango Guava Jive',
-		price: 6,
-		class: 'smoothie',
-		description: 'Put the mango in the guava and what do you get?',
-		itemNumber: 4
-	}
+    {
+        name: 'Grape Juice',
+        price: 4,
+        class: 'Juice',
+        description: 'The purest juice from delicious grapes.',
+        itemNumber: 1
+    },
+    {
+        name: 'Apple Juice',
+        price: 3,
+        class: 'Juice',
+        description: 'We squeezed an apple into a cup!',
+        itemNumber: 2
+    },
+    {
+        name: 'Kiwi Lime Blitz',
+        price: 5,
+        class: 'Smoothie',
+        description: 'Savory blend of kiwis, lime juice, and sorbet.',
+        itemNumber: 3
+    },
+    {
+        name: 'Mango Guava Jive',
+        price: 6,
+        class: 'Smoothie',
+        description: 'Put the mango in the guava and what do you get?',
+        itemNumber: 4
+    },
+    {   name: 'Pomegranate Passion',
+        price: 5,
+        class: 'Smoothie',
+        description: 'A passionate combination of pomegranate and pineapple.',
+        itemNumber: 5
+    },
+    {
+        name: 'Groovey Green Goddess ',
+        price: 4,
+        class: 'Juice',
+        description: 'Get your groove back with natures finest greens.',
+        itemNumber: 6
+    },
+    {
+        name: 'Pink Lady',
+        price: 4,
+        class: 'Juice',
+        description: 'Put some pink in your cheeks with the sweetest pink grapefruit.',
+        itemNumber: 7
+    },
+    {
+        name: 'Jungle Juice',
+        price: 6,
+        class: 'Juice',
+        description: 'The purest juice from spinach and tropical fruits',
+        itemNumber: 8
+    },
+    {
+        name: 'Pineapple Pleasure',
+        price: 5,
+        class: 'Smoothie',
+        description: 'If pineapple pleases you then this is the smoothie for you.',
+        itemNumber: 9
+    },
+    {
+        name: 'Pineapple Juice',
+        price: 4,
+        class: 'Juice',
+        description: 'The crazy crash of juice from delicious pineapple.',
+        itemNumber: 10
+    },
+    {
+        name: 'Happy Monday',
+        price: 7,
+        class: 'Juice',
+        description: 'Can make anyday of the week happy.',
+        itemNumber: 11
+    },
+    {
+        name: ' Berry Carrot Dream',
+        price: 4,
+        class: 'Juice',
+        description: 'The purest juice from delicious carrots.',
+        itemNumber: 12
+    }
 ];
 
 
@@ -42,8 +97,8 @@ masterItemList.forEach(function(item){
 //This function finds the info of each object, creates a container, fills it, and places it in the DOM.
 function printItemToShopPage(item) {
 	var itemContainer = $('<div></div>');//Creates Container
-	var namePTag = $('<p>'+item.name+'</p>');//Identifies Name
-	var pricePTag = $('<p>'+item.price+'</p>');//Identifies Price
+	var namePTag = $('<h4>'+item.name+'</h4>');//Identifies Name
+	var pricePTag = $('<p> $'+item.price+'</p>');//Identifies Price
 	var classPTag = $('<p>'+item.class+'</p><');//Identifies Class
 	var descriptionPTag = $('<p>'+item.description+'</p>');//Identifies Description
 	
