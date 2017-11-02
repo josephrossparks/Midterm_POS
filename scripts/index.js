0a1d87dc1ec9d66d7c0c3db0c263fe77c27d000d
Nicole King <kingnt78@yahoo.com>
2017-11-02 11:01:22 -0400

    messy files

--- scripts/index.js
+++ scripts/index.js
@@ -29,12 +29,77 @@ var masterItemList = [
 		class: 'smoothie',
 		description: 'Put the mango in the guava and what do you get?',
 		itemNumber: 4
+	},
+
+	{	name: 'Pomegranate Passion',
+		price: 5,
+		class: 'smoothie',
+		description: 'A passionate combination of pomegranate and pineapple.',
+		itemNumber: 5
+	},
+
+	{
+		name: 'Groovey Green Goddess ',
+		price: 4,
+		class: 'juice',
+		description: 'Get your groove back with natures finest greens.',
+		itemNumber: 6
+	},
+
+	{
+		name: 'Pink Lady',
+		price: 4,
+		class: 'juice',
+		description: 'Put some pink in your cheeks with the sweetest pink grapefruit.',
+		itemNumber: 7
+	},
+
+	{
+		name: 'Jungle Juice',
+		price: 6,
+		class: 'juice',
+		description: 'The purest juice from spinach and tropical fruits',
+		itemNumber: 8
+	},
+
+	{
+		name: 'Pineapple Pleasure',
+		price: 5,
+		class: 'smoothie',
+		description: 'If pineapple pleases you then this is the smoothie for you.',
+		itemNumber: 9
+	},
+
+	{
+		name: 'Pineapple Juice',
+		price: 4,
+		class: 'juice',
+		description: 'The crazy crash of juice from delicious pineapple.',
+		itemNumber: 10
+	},
+
+	{
+		name: 'Happy Monday',
+		price: 7,
+		class: 'juice',
+		description: 'Can make anyday of the week happy.',
+		itemNumber: 11
+	},
+
+	{
+		name: ' Berry Carrot Dream',
+		price: 4,
+		class: 'juice',
+		description: 'The purest juice from delicious carrots.',
+		itemNumber: 12
 	}
-];
+
+
+
 
 
 //This function prints all the items to the shop page.
-masterItemList.forEach(function(item){
+ masterItemList.forEach(function(item){
 	printItemToShopPage(item);
 });
 
