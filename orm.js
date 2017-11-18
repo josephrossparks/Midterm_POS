c4c9bc88b14f91be601b1b0124c536a55c20e7b8
Sloth I <github@slothwerks.com>
2017-11-18 14:08:35 -0500

    Added and tested retrieval, adding, updating, and deleting items from SQL database via API.

--- orm.js
+++ orm.js
@@ -1,5 +1,7 @@
 const pg = require("pg");
 
+// In orm.js we are separating out all of our inreractivity with PostgreSQL.
+
 const dbConfig = {
     user: "postgres",
     password: "p4r7n3r5",
@@ -11,11 +13,33 @@ const dbConfig = {
 
 const pool = new pg.Pool(dbConfig);
 
+// Here we are building an object that includes a series of functions which handle our database-related queries.
+// We will then pass these functions out of the module and require them within server.js.
+
 const orm = {
-	readAll: function(tableName) {
-		const sql = `SELECT * FROM ${tableName};`;
+	readAll: function(orderBy, tableName) {
+		const sql = `SELECT * FROM ${tableName} ORDER BY ${orderBy};`;
 		return pool.query(sql);
-	}
+	},
+
+    addMenuItem: function(newItem, tableName) {
+        const sql = `INSERT INTO ${tableName}(name, price, class, description, code) VALUES ($1,$2,$3,$4,$5);`;
+        const values = [newItem.name, newItem.price, newItem.class, newItem.description, newItem.code];
+        return pool.query(sql, values);
+    },
+
+    updateMenuItem: function(itemToUpdate, tableName) {
+        const sql = `UPDATE ${tableName} SET name=$1, price=$2, class=$3, description=$4 WHERE code=$5;`;
+        const values = [itemToUpdate.name, itemToUpdate.price, itemToUpdate.class, itemToUpdate.description, itemToUpdate.code];
+        return pool.query(sql, values);
+    },
+
+        deleteMenuItem: function(itemToDelete, tableName) {
+        const sql = `DELETE FROM ${tableName} WHERE code=$1;`;
+        const values = [itemToDelete.code];
+        return pool.query(sql, values);
+    }
+
 };
 
 module.exports = orm;
--- server.js
+++ server.js
@@ -8,17 +8,6 @@ const app = express();
 // The line below is used once the client has been built for live use.
 // app.use(express.static('client/build'));
 
-app.use(bodyParser.json());
-
-app.get('/api/menuitems', (req, res) => {
-	// res.send("stuff");
-	const tableName = "menu";
-	const dbResult = orm.readAll(tableName);
-	dbResult.then((result) => {
-		res.send(result.rows);
-	});
-});
-
 /* Internal memory database test...
 
 app.get('/api/menuitems', function(req, res) {
@@ -27,6 +16,46 @@ app.get('/api/menuitems', function(req, res) {
 });
 
 */
+
+app.use(bodyParser.json());
+
+// The following is the table name we will be using for our app queries.
+const tableName = "menu";
+
+app.get('/api/menuitems', (req, res) => {
+	let orderBy = "id"; // Future functionality will allow for sorting by code or other item properties; in this case orderBy would theoretically be passed in as a parameter.
+	const dbResult = orm.readAll(orderBy, tableName);
+	dbResult.then((result) => {
+		res.send(result.rows);
+	});
+});
+
+app.post('/api/menuitems', (req, res) => {
+  const newItem = req.body;
+  const dbResult = orm.addMenuItem(newItem, tableName);
+  dbResult.then(result => {
+    res.status(201);
+    res.send('SUCCESS');
+  });
+});
+
+app.put('/api/menuitems', function(req, res) {
+	const itemToUpdate = req.body;
+    const dbResult = orm.updateMenuItem(itemToUpdate, tableName);
+  	dbResult.then(result => {
+	    res.status(201);
+	    res.send('SUCCESS');
+    });
+});
+
+app.delete('/api/menuitems', function(req, res) {
+	const itemToDelete = req.body;
+    const dbResult = orm.deleteMenuItem(itemToDelete, tableName);
+  	dbResult.then(result => {
+	    res.status(201);
+	    res.send('SUCCESS');
+    });
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
